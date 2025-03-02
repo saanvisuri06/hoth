5b0c01ceaf2cbb8d431fb302c464d2942e8aa878
saanvisuri06 <saanvisuri06@g.ucla.edu>
2025-03-02 10:32:06 -0800

    added home page

--- hoth_kindness/src/App.jsx
+++ hoth_kindness/src/App.jsx
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from './layout/MainLayout';
 import Login from './Login';
+import HomePage from './HomePage';
 
 const App = () => {
   const router = createBrowserRouter([{ 
       path: "",
       element: <MainLayout />, 
-      children: [{path: "/", element: <Login />}, 
+      children: [{path: "/", element: <Login />}, {path: "/home", element: <HomePage />}
       ]}]);
 
 return (
