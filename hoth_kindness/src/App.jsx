9b0fe444f4ce885bd08fd84cdcb2dcfbc19a37d1
Iris Li <yushanli54@gmail.com>
2025-03-02 10:18:29 -0800

    Your commit message here

---
+++ hoth_kindness/src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from './layout/MainLayout';
+import Login from './Login';
+
+const App = () => {
+  const router = createBrowserRouter([{ 
+      path: "",
+      element: <MainLayout />, 
+      children: [{path: "/", element: <Login />}, 
+      ]}]);
+
+return (
+  <RouterProvider router={router} />
+);
+}
+
+export default App
+
