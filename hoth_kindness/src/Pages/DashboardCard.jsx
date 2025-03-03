ab9df60df17f07a0934849a8e8f615c039e7d298
Renxin Li <2598071104@qq.com>
2025-03-02 16:38:05 -0800

    feed + dashboard changes

--- hoth_kindness/src/Pages/DashboardCard.jsx
+++ hoth_kindness/src/Pages/DashboardCard.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 const DashboardCard = ( {title, value, hashtags = []}) => {
     return (
-    <div class="mx-auto w-full overflow-hidden rounded-xl bg-white shadow-md">
-        <div class="p-8">
+    <div class="mx-auto w-full min-h-[300px] overflow-hidden rounded-xl bg-white shadow-lg flex flex-col">
+        <div class="p-8 flex flex-col flex-grow">
             <div class="text-sm font-semibold tracking-wide text-indigo-500 uppercase text-center">{title}</div>
             <a href="#" class="mt-1 block text-lg leading-tight font-medium text-black hover:underline">
                 {title}
             </a>
-            <p class="mt-2 text-gray-500 font-itim">
+            <p class="mt-2 text-gray-500 font-itim flex-grow">
                 {value}
             </p>
             {/* Map through the hashtags array and render each Hashtag component */}
--- hoth_kindness/src/layout/MainLayout.jsx
+++ hoth_kindness/src/layout/MainLayout.jsx
@@ -5,10 +5,6 @@ const MainLayout = () => {
 
   return (
     <>
-<<<<<<< HEAD
-    <div className='font-itim'>Layout</div>
-=======
->>>>>>> f779128043ee2d625db21b9d53451aab4d363035
     <Outlet />
     </>
   )
